b1957676e51b0c856aae45ebc7f311439fbe3fc9
Sir Claudius <actuallymentor/sir-claudius@github.com>
2026-03-09 12:42:21 +0000

    ⚡ Auto-reload when new PWA version activates

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,10 @@
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 
 import App from './App'
 import './index.css'
+
+// Auto-update: when a new SW activates, reload to apply immediately
+registerSW( { onNeedRefresh: () => location.reload() } )
 
 createRoot( document.getElementById( `root` ) ).render( <App /> )
